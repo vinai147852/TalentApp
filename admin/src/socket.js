d0eb2ac6aab6bed2267b6bd1ab7045d1bb71e80d
Vinay Chitimireddy <Vinai147852@gmail.com>
2026-07-10 07:45:22 -0700

    port changes for admin

--- admin/src/socket.js
+++ admin/src/socket.js
@@ -2,5 +2,5 @@ import io from "socket.io-client";
 const ENDPOINT =
   process.env.NODE_ENV === "production"
     ? "https://tjcfdbv2-5005.inc1.devtunnels.ms/"
-    : "http://localhost:5005/";
+    : `http://${window.location.hostname}:5005/`;
 export default io(ENDPOINT);
